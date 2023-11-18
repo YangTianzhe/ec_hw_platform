64a6cb6d73812af11591c1e2b7f06c08a037e437
Yang Tianzhe <ytz2005@sjtu.edu.cn>
2023-11-18 22:09:56 +0800

    完成PID类

---
+++ Inc/pid.hpp
@@ -0,0 +1,26 @@
+//
+// Created by ytz20 on 2023/11/18.
+//
+
+#ifndef PROJECT_PID_HPP
+#define PROJECT_PID_HPP
+
+class PID
+{
+public:
+    PID(float kp, float ki, float kd, float i_max, float out_max);
+
+    float Calculate(float ref, float fdb);
+
+private:
+    float kp_, ki_, kd_;
+    float i_max_, out_max_;//积分限幅,输出限幅
+    float output_;
+
+protected:
+    float ref_, fdb_;//目标，当前
+    float err_, err_sum_, last_err_;
+    float pout_, iout_, dout_;
+};
+
+#endif //PROJECT_PID_HPP
---
+++ Src/pid.cpp
@@ -0,0 +1,32 @@
+//
+// Created by ytz20 on 2023/11/18.
+//
+
+#include "../Inc/pid.hpp"
+#include "../Inc/my_math.h"
+
+PID::PID(float kp, float ki, float kd, float i_max, float out_max)
+{
+    kp_=kp;
+    ki_=ki;
+    kd_=kd;
+    i_max_=i_max;
+    out_max_=out_max;
+}
+
+float PID::Calculate(float ref, float fdb)
+{
+    ref_=ref;
+    fdb_=fdb;
+
+    last_err_=err_;
+    err_=ref_-fdb_;
+    err_sum_=Limit(err_sum_+err_,-i_max_,i_max_);
+
+    pout_=kp_*err_;
+    iout_=ki_*err_sum_;
+    dout_=kd_*(last_err_-err_);
+
+    output_=Limit(pout_+iout_+dout_,-out_max_,out_max_);
+    return output_;
+}
